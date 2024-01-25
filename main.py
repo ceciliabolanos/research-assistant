ac487f440fa9e9d90111437f463ed3e52ad21c4c
Cecilia Bolaños <72699426+bcecilia1@users.noreply.github.com>
2024-01-25 20:33:39 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import argparse
 import os
 import json
-from extract_code import get_project_structure, analyze_dependencies, process_atlas_data
+from extract_code import get_project_structure
 from code_search import CodeSearcher
+from tokens import string_code_to_sequence
 
 
+#C:/Users/chech/miniconda3/python.exe c:/Users/chech/Documents/TPNLP/CodeBERT-master/research-assistant/main.py --project_path c:/Users/chech/Documents/TPNLP/CodeBERT-master/CodeBert --output_dir_path c:/Users/chech/Documents/TPNLP/CodeBERT-master/CodeBert/prueba
 def main():
     parser = argparse.ArgumentParser(description='Parse a project structure into a JSON representation.')
     parser.add_argument('--project_path', type=str, required=True, help='Path to the root of the project.')
     parser.add_argument('--output_dir_path', type=str, required=True, help='Path to the output dir.')
-    parser.add_argument('--model_path', type=str, default='C:\\Users\\chech\\Documents\\TPNLP\\CodeBERT-master\\RA\\model (7).bin', help='Path to unixcoder model') # change to your model path
+    parser.add_argument('--model_path', type=str, default='C:\\Users\\chech\\Documents\\TPNLP\\CodeBERT-master\\research-assitant\\unixcoder-ft.bin', help='Path to unixcoder model')
     parser.add_argument('--nl_query', type=str, default='need to know the loss function of seq2seq model')
     args = parser.parse_args()
 
@@ -24,6 +26,7 @@ def main():
         json.dump(project_structure, f, ensure_ascii=False, indent=4)
     
     data = {}
+
     # Leer y analizar el archivo JSON
     try:
        with open(output_path, 'r') as file:
@@ -31,18 +34,25 @@ def main():
     except Exception as e:
         print(f"Error al leer el archivo JSON: {e}")
 
-    # Proceso de extracción de fragmentos de código
     code_snippets = []
+    for main_key in data.keys():
+        for filename, content in data[main_key].items():
+            if 'functions' in content:
+                for function_name, function_content in content['functions'].items():
+                    if 'code' in function_content:
+                        function_content['code'] = string_code_to_sequence(function_content['code'])
+                        code_snippets.append(function_content['code'])
+                   # if 'docstring' in function_content:
+                   #     function_content['docstring'] = modify_docstring(function_content['docstring'])
+
+    # Escribir los datos modificados en un nuevo archivo JSON
+    new_output_path = 'path_a_tu_nuevo_archivo.json'  # Cambia esto por la ruta de tu nuevo archivo
     try:
-    # Iterar sobre todas las claves principales del diccionario JSON
-        for main_key in data.keys():
-            for filename, content in data[main_key].items():
-                if 'functions' in content:
-                    for function_name, function_content in content['functions'].items():
-                        if 'code' in function_content:
-                            code_snippets.append(function_content['code'])
+        with open(new_output_path, 'w') as file:
+            json.dump(data, file, indent=4)
     except Exception as e:
-        code_snippets = f"Error al procesar el JSON: {e}"
+        print(f"Error al escribir el archivo JSON: {e}")
+   
 
     searcher = CodeSearcher(args.model_path, code_snippets)
     k=3
---
+++ tokens.py
@@ -0,0 +1,59 @@
+import ast
+from DFG import DFG_python,DFG_java,DFG_ruby,DFG_go,DFG_php,DFG_javascript
+from utils import (remove_comments_and_docstrings,
+                   tree_to_token_index,
+                   index_to_code_token,
+                   tree_to_variable_index,
+                   extract_dataflow)
+from tree_sitter import Language, Parser
+
+dfg_function={
+    'python':DFG_python,
+    'java':DFG_java,
+    'ruby':DFG_ruby,
+    'go':DFG_go,
+    'php':DFG_php,
+    'javascript':DFG_javascript
+}
+
+parsers={}        
+for lang in dfg_function:
+    LANGUAGE = Language('./my-languages.so', lang)   # Probablemente cambiarlo?
+    parser = Parser()
+    parser.set_language(LANGUAGE) 
+    parser = [parser,dfg_function[lang]]    
+    parsers[lang]= parser
+
+def extract_ast_from_code(source_code):
+    return ast.parse(source_code)
+
+# provienen de convert_examples_to_features
+def string_code_to_sequence(source_code):
+    code_tokens,dfg = extract_dataflow(source_code, parser,"python")[0]
+    #code_tokens=[tokenizer.tokenize('@ '+x)[1:] if idx!=0 else tokenizer.tokenize(x) for idx,x in enumerate(code_tokens)]
+    #ori2cur_pos={}
+    #ori2cur_pos[-1]=(0,0)
+    #for i in range(len(code_tokens)):
+    #    ori2cur_pos[i]=(ori2cur_pos[i-1][1],ori2cur_pos[i-1][1]+len(code_tokens[i]))    
+    #code_tokens=[y for x in code_tokens for y in x]  
+    ##truncating
+    #code_tokens=code_tokens[:args.code_length+args.data_flow_length-2-min(len(dfg),args.data_flow_length)]
+    #code_tokens =[tokenizer.cls_token]+code_tokens+[tokenizer.sep_token]
+    #code_ids =  tokenizer.convert_tokens_to_ids(code_tokens)
+    #position_idx = [i+tokenizer.pad_token_id + 1 for i in range(len(code_tokens))]
+    #dfg=dfg[:args.code_length+args.data_flow_length-len(code_tokens)]
+    #code_tokens+=[x[0] for x in dfg]
+    #position_idx+=[0 for x in dfg]
+    #code_ids+=[tokenizer.unk_token_id for x in dfg]
+    #padding_length=args.code_length+args.data_flow_length-len(code_ids)
+    #position_idx+=[tokenizer.pad_token_id]*padding_length
+    #code_ids+=[tokenizer.pad_token_id]*padding_length      
+    return code_tokens
+
+def string_nl_to_sequence(source_nl):
+    #nl_tokens=tokenizer.tokenize(nl)[:args.nl_length-2]
+    #nl_tokens =[tokenizer.cls_token]+nl_tokens+[tokenizer.sep_token]
+    #nl_ids =  tokenizer.convert_tokens_to_ids(nl_tokens)
+    #padding_length = args.nl_length - len(nl_ids)
+    #nl_ids+=[tokenizer.pad_token_id]*padding_length
+    return   
