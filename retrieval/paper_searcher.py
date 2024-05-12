5c9e72372b18a5ebc1c3959094b41f1c6e951d84
Juan Wisznia <juanwisznia@gmail.com>
2024-05-12 19:44:34 -0300

    hardcoding retrieval model for papersearch

--- retrieval/paper_searcher.py
+++ retrieval/paper_searcher.py
@@ -2,13 +2,15 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 from typing import List, Dict, Any, Optional, Union
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoTokenizer
 from langchain.vectorstores import FAISS
 from retrieval.searcher import Searcher
 
 class PaperSearcher(Searcher):
-    def __init__(self, model_path: str, device: Optional[torch.device] = None):
-        super().__init__(model_path, device)
+    def __init__(self, device: Optional[torch.device] = None):
+        super().__init__(device)
+        model_path = 'Alibaba-NLP/gte-large-en-v1.5'  # Hardcoded model path
+        self.model = self.load_model(model_path)
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
 
     def load_model(self, model_path: str) -> AutoModel:
@@ -32,6 +34,7 @@ class PaperSearcher(Searcher):
             self.build_faiss_index()
 
         scores, indices = self.faiss_index.search(query_embedding, k)
+
         results = []
         for i, index in enumerate(indices):
             result = {
@@ -44,8 +47,8 @@ class PaperSearcher(Searcher):
         return results
 
     @classmethod
-    def load_from_disk(cls, folder_path: str, model_path: str, device: Optional[torch.device] = None):
-        searcher = cls(model_path, device)
+    def load_from_disk(cls, folder_path: str, device: Optional[torch.device] = None):
+        searcher = cls(device)
         searcher.faiss_index = FAISS.load_local(folder_path=folder_path, embeddings_dimension=searcher.model.config.hidden_size)
         searcher.items = searcher.faiss_index.index_to_docstore_id.keys()
         searcher.metadatas = searcher.faiss_index.index_to_metadata.values() if searcher.faiss_index.index_to_metadata else None
